c84539be01d919369e29061eb54ef16e1bb1a87e
Anatolii <anatolii.mladshii@mail.ru>
2025-11-26 18:30:59 +0300

    Добавлено основное меню

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Метод для кнопки "НАЧАТЬ"
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Метод для кнопки "ВЫХОД"
+    public void ExitGame()
+    {
+        Debug.Log("Игра Закрылась");
+        Application.Quit();
+    }
+}
